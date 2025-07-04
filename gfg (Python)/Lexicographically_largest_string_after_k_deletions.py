52466d26dd3493039a71faf514043bfe79200d3d
Amit-AK-07 <amitkoyal2000@gmail.com>
2025-07-04 21:41:24 +0530

    04-07-2025

---
+++ gfg (Python)/Lexicographically_largest_string_after_k_deletions.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSubseq(self, s, k):
+        #code here
+        result = []
+
+        for ch in s:
+            while result and result[-1] < ch and k > 0:
+                result.pop()
+                k -= 1
+            result.append(ch)
+
+        while k > 0:
+            result.pop()
+            k -= 1
+
+        return ''.join(result)
